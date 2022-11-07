e06bf00aa80074d86412f6d63e20b16e59724c12
Daniel Castaño Jaramillo <drcastanoj@gmail.com>
2022-11-07 17:48:23 -0500

    fix async await

--- taks1/task1.2.js
+++ taks1/task1.2.js
@@ -2,19 +2,19 @@ const csv = require('csvtojson')
 const fs = require('fs')
 const csvFilePath = './csv/nodejs-hw1-ex1.csv';
 
-function main() {
-    csv({ delimiter: ';' }).
+async function main() {
+    const jsonObj = await csv({ delimiter: ';' }).
         preFileLine((fileLine, idx) => (idx === 0) ? fileLine.toLowerCase() : fileLine)
-        .fromFile(csvFilePath, {})
-        .then((jsonObj) => {
-            const file = fs.createWriteStream('./csvToText.txt', {
-                flags: 'a',
-            })
-            jsonObj.forEach(book => {
-                file.write(JSON.stringify(book) + '\n');
-            });
-            file.close();
-        });
+        .fromFile(csvFilePath, {});
+
+    const file = fs.createWriteStream('./csvToText.txt', {
+        flags: 'a',
+    })
+    jsonObj.forEach(book => {
+        file.write(JSON.stringify(book) + '\n');
+    });
+    file.close();
+
 }
 
 main();
